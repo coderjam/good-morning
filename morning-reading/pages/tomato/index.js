cd66fc20558228289206435820d3a76e5534da10
邓健 <jdeng@senparc.com>
2020-01-02 10:12:05 +0800

    一些页面

---
+++ morning-reading/pages/tomato/index.js
@@ -0,0 +1,75 @@
+// pages/tomato/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [{
+      userId: "1", 
+      title: "看书",
+      content: "10天看完《小王子》",
+      address: "苏州市姑苏区苏州大学",
+      startTime: "2019-09-15",
+      endTime:"2019-12-15",
+      open: true,
+      count:0
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
